279c1e03e22692c462c2fac8cce72bd5d3eafe47
Brian <briankingsbury1@gmail.com>
2025-03-01 13:03:32 -0600

    Nav adjustment

--- portfolio/src/components/app-nav/app-nav.tsx
+++ portfolio/src/components/app-nav/app-nav.tsx
@@ -13,9 +13,9 @@ export class AppNav {
       <nav class="top-nav">
         <p class="me">Brian Kingsbury</p>
         <div class="links">
-            <a class={{'active': activePath === '/about'}} href="../about">About</a>
-            <a class={{'active': activePath === '/work'}} href="../work">Work</a>
-            <a class={{'active': activePath === '/experience'}} href="../experience">Experience</a>
+            <a class={{'active': activePath === '/about'}} href="/about">About</a>
+            <a class={{'active': activePath === '/work'}} href="/work">Work</a>
+            <a class={{'active': activePath === '/experience'}} href="/experience">Experience</a>
             {/* <button>
               <img src="../../assets/settings.svg" alt="settings icon"/>
             </button> */}
