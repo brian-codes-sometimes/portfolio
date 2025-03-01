67304704ea6c8e9b2b0807b09cbdc45476e1280e
Brian <briankingsbury1@gmail.com>
2025-02-28 21:00:41 -0600

    Fix random text

--- portfolio/src/components/app-nav/app-nav.tsx
+++ portfolio/src/components/app-nav/app-nav.tsx
@@ -13,7 +13,7 @@ export class AppNav {
       <nav class="top-nav">
         <p class="me">Brian Kingsbury</p>
         <div class="links">
-            <a class={{'active': activePath === '/about'}} href="../about" ex>About</a>
+            <a class={{'active': activePath === '/about'}} href="../about">About</a>
             <a class={{'active': activePath === '/work'}} href="../work">Work</a>
             <a class={{'active': activePath === '/experience'}} href="../experience">Experience</a>
             {/* <button>
